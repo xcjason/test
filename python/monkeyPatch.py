de3c5bbadfeb671bccf12a8b6422473a3089c0cb
Ubuntu 12.04 <jason@ubuntu.(none)>
2015-05-17 21:21:32 -0500

    commit

--- python/monkeyPatch.py
+++ python/monkeyPatch.py
@@ -11,11 +11,13 @@ class MonkeyTest(object):
         return x * y
 
 monkey = MonkeyTest()
+monkey2 = MonkeyTest()
 print monkey.add(16, 30)
 print MonkeyTest.multi(4, 5)
 lbd = lambda x, y: 2000
 oldAdd = monkey.add
 monkey.add = lbd
 print 'after monkey patch: ', monkey.add(16, 30)
+print 'another object: ', monkey2.add(16, 30)
 monkey.add = oldAdd
 print 'restore to old function: ', monkey.add(16, 30)
