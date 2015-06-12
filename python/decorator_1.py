00cbe9c67e300fd7c68c21537aa2b57144dc1b5a
xcjason <xucong.jason@gmail.com>
2015-06-12 13:40:39 -0500

    all

--- python/decorator_1.py
+++ python/decorator_1.py
@@ -7,14 +7,16 @@ def compose_greet_function(name):
 greet = compose_greet_function('John')
 print greet()
 
-def get_text(name):
-   return "lorem ipsum, {0} dolor sit amet".format(name)
-
 def p_decorate(func):
    def func_wrapper(name):
        return "<p>{0}</p>".format(func(name))
    return func_wrapper
 
-my_get_text = p_decorate(get_text)
+@p_decorate
+def get_text(name):
+   return "lorem ipsum, {0} dolor sit amet".format(name)
 
-print my_get_text("John")
+print get_text('John')
+
+# my_get_text = p_decorate(get_text)
+# print my_get_text("John")
